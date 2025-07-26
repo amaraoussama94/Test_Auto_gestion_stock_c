d23f676c442907d13ba79c013e9ce29cfc85349c
Oussama Amara <wassa36@yahoo.fr>
2025-07-26 16:43:53 +0100

    Update test order to avoid errors

--- meta/meta_TEST_REGISTRY.py
+++ meta/meta_TEST_REGISTRY.py
@@ -1,10 +1,10 @@
 TEST_REGISTRY = {
     "Theem.py": {"run": False, "type": "non-test"},
+    "smoke_test.py": {"run": True, "type": "ci"},
     "add_prod_test.py": {"run": True, "type": "ci"},
-    "delete_prod_test.py": {"run": True, "type": "ci"},
-    "full_journey_test.py": {"run": False, "type": "weekly"},
     "list_prod_test.py": {"run": True, "type": "ci"},
     "modify_prod_test.py": {"run": True, "type": "ci"},
+    "delete_prod_test.py": {"run": True, "type": "ci"},
+    "full_journey_test.py": {"run": False, "type": "weekly"},
     "regression_bug_test.py": {"run": True, "type": "ci"},
-    "smoke_test.py": {"run": True, "type": "ci"},
 }
